774f6a9257f3b6bf9ea6ca85ef16a6b8f7118431
BrooksLee <743703241@qq.com>
2014-10-12 21:55:18 +0800

    add the function MaxHeapDelectKey to the  C_data_struct/project/src/max_heap.c

--- C_date_struct/project/src/max_heap.c
+++ C_date_struct/project/src/max_heap.c
@@ -6,9 +6,14 @@
    .NodeList = malloc(sizeof(MaxHeapNode)*__nodes), \
    .CurrentSize = 0, \
    .MaxSize = __nodes,}
-#define DEBUG_CHECK(__condiction,__ret) \
+#define DEBUG_CHECK_RETURN(__condiction,__ret) \
 	if ((__condiction)) \
 	{ printf(""#__condiction" is not meeted\n return : "#__ret" \n"); return (__ret);}
+#define DEBUG_CHECK(__condiction) \
+	if ((__condiction)) printf(""#__condiction" is failed! \n")
+#define DEBUG_ASSERT(__condiction) \
+	if ((__condiction)) printf(""#__condiction" is failed!\n");\
+	assert((__condiction))
 
 typedef struct{
     int key;    
@@ -24,7 +29,7 @@ int MaxHeapInsertNode(MaxHeap *Heap,
 			MaxHeapNode Node)
 {
     int Site;
-    DEBUG_CHECK(Heap->CurrentSize + 1 > Heap->MaxSize, -1);
+    DEBUG_CHECK_RETURN(Heap->CurrentSize + 1 > Heap->MaxSize, -1);
 
     Site = ++Heap->CurrentSize;
     while ((Site != 1) && Node.key > Heap->NodeList[Site/2].key ){
@@ -38,14 +43,37 @@ int MaxHeapInsertNode(MaxHeap *Heap,
 
 int MaxHeapInsertKey(MaxHeap *Heap, int Key)
 {
-     DEBUG_CHECK(Heap->CurrentSize + 1 > Heap->MaxSize, -1);
+     DEBUG_CHECK_RETURN(Heap->CurrentSize + 1 > Heap->MaxSize, -1);
      MaxHeapNode Node = {.key = Key};
      MaxHeapInsertNode(Heap,Node);
      return 0;
 }
 
+int MaxHeapDelectKey(MaxHeap *Heap)
+{
+    DEBUG_CHECK(!Heap || !Heap->CurrentSize);
+
+    int MaxIterm = Heap->NodeList[1].key;
+    int LastIterm = Heap->NodeList[Heap->CurrentSize].key;
+    int parent = 1;
+    int child = 2;
+
+    while (child < Heap->CurrentSize){
+        if (Heap->NodeList[child].key < Heap->NodeList[child + 1].key)
+	    child++;
+        Heap->NodeList[parent] =  Heap->NodeList[child];
+	parent = child;
+	child *= 2; 
+    }
+    Heap->NodeList[parent].key = LastIterm;
+    return MaxIterm;
+}
+
+
 void main(void)
 {
    MaxHeapCreat(Heap,10);
    MaxHeapInsertKey(&Heap, 20);
+   printf("the max iterm is : %d \n",MaxHeapDelectKey(&Heap));
+
 }
